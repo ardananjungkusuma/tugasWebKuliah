82eb42e8771f085e2b9ab35ee707efb71606fea3
ardananjungkusuma <ardananjungkusuma@gmail.com>
2019-10-19 21:17:52 +0700

    backup

--- soal_latihan/latihan1/1.php
+++ soal_latihan/latihan1/1.php
@@ -1,26 +1,28 @@
-<?php
-$catService = $_POST['catService'];
-$catEat = $_POST['catEat'];
-$datepick = $_POST['datepick'];
-$nama = $_POST['nama'];
-$phone_number = $_POST['phone_number'];
-$message = $_POST['message'];
-$email = $_POST['email'];
+<!DOCTYPE html>
+<html>
+    <head><title>Output Customer</title></head>
+    <body>
+        <h3>This is the fuckin final result</h3>
+        <?php
+            $catService = $_POST['catService'];
+            $catEat = $_POST['catEat'];
+            $datepick = $_POST['datepick'];
+            $nama = $_POST['nama'];
+            $phone_number = $_POST['phone_number'];
+            $message = $_POST['message'];
+            $email = $_POST['email'];
 
-echo "Service : ".$catService."<br>";
-echo "Eat : ";
-foreach($catEat as $catEat){
-    echo($catEat." ");
-}
-echo "<br>";
-// if(!empty($_POST['catEat'])){
-//     foreach($_POST['catEat'] as $selected){
-//     echo "Eat : "$selected."<br>";
-//     }
-// }
-echo "Date : ".$datepick."<br>";
-echo "Name : ".$nama."<br>";
-echo "Phone Number : ".$phone_number."<br>";
-echo "Message : ".$message."<br>";
-echo "Email : ".$email."<br>";
-?>
+            echo "Service : ".$catService."<br>";
+            echo "Eat : ";
+            foreach($catEat as $catEat){
+                echo($catEat." ");
+            }
+            echo "<br>";
+            echo "Date : ".$datepick."<br>";
+            echo "Name : ".$nama."<br>";
+            echo "Phone Number : ".$phone_number."<br>";
+            echo "Message : ".$message."<br>";
+            echo "Email : ".$email."<br>";
+        ?>
+    </body>
+</html>
