38a1a01dedf219c9108681873c4fc917bf12fa47
ardananjungkusuma <ardananjungkusuma@gmail.com>
2019-11-04 08:35:00 +0700

    upload

---
+++ phpJs9/getForm.php
@@ -0,0 +1,13 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+            if(isset($_GET["username"])AND isset($_GET["email"])){
+                echo "Welcome ".$_GET["username"] ."!<br>";
+                echo "Your email address is : ".$_GET["email"];
+            }else{
+                echo "Maaf anda harus mengakses halaman ini dari form1.html";
+            }
+        ?>
+    </body>
+</html>
---
+++ phpJs9/postForm.php
@@ -0,0 +1,7 @@
+<html>
+    <head></head>
+    <body>
+        Welcome <?php echo $_POST["username"];?> !! <br>
+        Your Email Address is : <?php echo $_POST["email"];?>
+    </body>
+</html>
